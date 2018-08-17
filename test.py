401dcae77cab087d9efb0619d44df6151da4a8be
prantikair972 <38919837+prantikair972@users.noreply.github.com>
2018-08-18 01:01:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,58 +1,36 @@
-import matplotlib.pyplot as plt 
+
 import math
-# k=[2,3,4,10,11,12,20,25,30]
-# a=k[0]
-# b=k[len(k)-1]
-# cluster_a=list()
-# cluster_b=list()
-# for i in range(len(k)):
-# 	if abs(k[i]-a)<abs(k[i]-b):
-# 		cluster_a.append(k[i])
-# 	else:
-# 		cluster_b.append(k[i])
-# while sum(cluster_a)/len(cluster_a) !=a and sum(cluster_b)/len(cluster_b)!=b:
-# 	a=sum(cluster_a)/len(cluster_a)
-# 	b=sum(cluster_b)/len(cluster_b)
-# 	cluster_a=list()
-# 	cluster_b=list()
-# 	for i in range(len(k)):
-# 		if abs(k[i]-a)<abs(k[i]-b):
-# 			cluster_a.append(k[i])
-# 		else:
-# 			cluster_b.append(k[i])
-# print(a)
-# print(b)
-# print(cluster_a)
-# print(cluster_b)
-dataset=[[1,2],[2,2],[2,1],[-1,4],[-2,-1],[-1,-1]]
-mean1=dataset[0]
-mean2=dataset[len(dataset)-1]
-cluster_1=list()
-cluster_2=list()
+
+dataset=[[1,2],[2,2],[2,1],[-1,4],[-2,-1],[-1,-1]] #This is going to be our sample dataset
+mean1=dataset[0] #Initializing First Mean/Centroid
+mean2=dataset[len(dataset)-1] #Initializing Second Mean/Centroid
+cluster_1=list()#First cluster
+cluster_2=list()#First cluster
 
 def E_distance(a,b):
-	return math.pow(math.pow(a[1]-b[1],2)+math.pow(a[0]-b[0],2),0.5)
+	return math.pow(math.pow(a[1]-b[1],2)+math.pow(a[0]-b[0],2),0.5) #computing the euclidean Distance between two points
 for i in dataset:
 	if E_distance(i,mean1)<E_distance(i,mean2):
-		cluster_1.append(i)
+		cluster_1.append(i)#Clustering the elements
 	else:
-		cluster_2.append(i)
-def average(a):
+		cluster_2.append(i)#Clustering the elements
+def average(a):    #Used for finding the centroid of all the points belonging to that cluster
 	centroid=[0,0]
 	centroid[0]=sum(a[i][0] for i in range(len(a)))/len(a)
 	centroid[1]=sum(a[i][1] for i in range(len(a)))/len(a)
 	return centroid
-while average(cluster_1)!=mean1 and average(cluster_2)!=mean2:
-	mean1=average(cluster_1)
-	mean2=average(cluster_2)
+while average(cluster_1)!=mean1 and average(cluster_2)!=mean2:#Conition for stopping  the KMeans Algorithms
+	
+	mean1=average(cluster_1)#the first new mean/centroid
+	mean2=average(cluster_2)#the second new mean/centroid
 	cluster_1=list()
 	cluster_2=list()
 	for i in dataset:
 		if E_distance(i,mean1)<E_distance(i,mean2):
-			cluster_1.append(i)
+			cluster_1.append(i)#Forming the new cLuster
 		else:
-			cluster_2.append(i)
-print(mean1)
+			cluster_2.append(i)#Forming the new cluster
+print(mean1)			
 print(mean2)
 print(cluster_1)
 print(cluster_2)
